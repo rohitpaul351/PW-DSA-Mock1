7149ef7981837a8d2181c777b55e9b78d5675425
rohitpaul351 <77479707+rohitpaul351@users.noreply.github.com>
2023-06-25 22:34:00 +0530

    Add files via upload

---
+++ firstUniqueChar.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class firstUniqueChar{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String s = sc.next();
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            if(!map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),1);
+            }
+            else{
+                map.put(s.charAt(i),(map.get(s.charAt(i))+1));
+            }
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(map.get(s.charAt(i)) == 1){
+                System.out.print(i);
+                return;
+            }
+        }
+    }	
+}
